2c7a7fa0ccd5de10cc3a7a238369e63715a52ada
Julian Tejera <juliantejera@outlook.com>
2015-03-21 12:04:35 -0400

    Implemented dictionary mapping for fuel

--- DominicanFuel/Fuel.swift
+++ DominicanFuel/Fuel.swift
@@ -8,7 +8,7 @@
 
 import Foundation
 
-class Fuel {
+class Fuel: DictionaryMapping {
     var id = 0
     var name = ""
     var price = 0.0
@@ -20,4 +20,36 @@ class Fuel {
         self.price = price
         self.publishedAt = publishedAt
     }
+    
+    required init(dictionary: [NSObject: AnyObject]) {
+        if let id = dictionary[Fuel.kId()] as? Int {
+            self.id = id
+        }
+        
+        if let name = dictionary[Fuel.kName()] as? String {
+            self.name = name
+        }
+        
+        if let price = dictionary[Fuel.kPrice()] as? Double {
+            self.price = price
+        }
+        
+        if let publishedAt = dictionary[Fuel.kPrice()] as? String {
+            self.publishedAt = NSDateFormatter().dateFromString(publishedAt)
+        }
+    }
+    
+    func toDictionary() -> [NSObject: AnyObject] {
+        var dictionary = [String: AnyObject]()
+        dictionary[Fuel.kId()] = id
+        dictionary[Fuel.kName()] = name
+        dictionary[Fuel.kPrice()] = price
+        dictionary[Fuel.kPublishedAt()] = publishedAt
+        return dictionary
+    }
+    
+    class func kId() -> String { return "id" }
+    class func kName() -> String { return "name" }
+    class func kPrice() -> String { return "price" }
+    class func kPublishedAt() -> String { return "publishedAt" }
 }
--- DominicanFuelTests/FuelTests.swift
+++ DominicanFuelTests/FuelTests.swift
@@ -11,10 +11,13 @@ import XCTest
 
 class FuelTests: XCTestCase {
     var fuel: Fuel!
-
+    var mappedDictionary: [NSObject: AnyObject]!
+    var mappedFuel: Fuel!
     override func setUp() {
         super.setUp()
         fuel = Fuel(id: 1, name: "Gasolina Premium", price: 222.52, publishedAt: NSDate(timeIntervalSince1970: 0))
+        mappedDictionary = fuel.toDictionary()
+        mappedFuel = Fuel(dictionary: mappedDictionary)
     }
     
     override func tearDown() {
@@ -42,6 +45,73 @@ class FuelTests: XCTestCase {
     func testInitPublishedAt() {
         let actual = fuel.publishedAt!
         let expected = NSDate(timeIntervalSince1970: 0)
+        XCTAssertEqual(actual, expected, "Expected '\(actual)' to be '\(expected)'")
+    }
+    
+    func testConstant_kId() {
+        XCTAssertEqual(Fuel.kId(), "id")
+    }
+    
+    func testConstant_kName() {
+        XCTAssertEqual(Fuel.kName(), "name")
+    }
+    
+    func testConstant_kPrice() {
+        XCTAssertEqual(Fuel.kPrice(), "price")
+    }
+    
+    func testConstant_kPublishedAt() {
+        XCTAssertEqual(Fuel.kPublishedAt(), "publishedAt")
+    }
+    
+    
+    func testInitWithDictionary_Id() {
+        let actual = mappedFuel.id
+        let expected = 1
+        XCTAssertEqual(actual, expected, "Expected '\(actual)' to be '\(expected)'")
+    }
+    
+    func testInitWithDictionary_Name() {
+        let actual = mappedFuel.name
+        let expected = "Gasolina Premium"
+        XCTAssertEqual(actual, expected, "Expected '\(actual)' to be '\(expected)'")
+    }
+    
+    func testInitWithDictionary_Price() {
+        let actual = mappedFuel.price
+        let expected = 222.52
+        XCTAssertEqual(actual, expected, "Expected '\(actual)' to be '\(expected)'")
+    }
+    
+//    func testInitWithDictionary_PublishedAt() {
+//        let actual = mappedFuel.id
+//        let expected = 1
+//        XCTAssertEqual(actual, expected, "Expected '\(actual)' to be '\(expected)'")
+//    }
+    
+    func testToDictionary_Id() {
+        let actual = mappedDictionary[Fuel.kId()] as Int
+        let expected = 1
+        XCTAssertEqual(actual, expected, "Expected '\(actual)' to be '\(expected)'")
+    }
+    
+    func testToDictionary_Name() {
+        let actual = mappedDictionary[Fuel.kName()] as String
+        let expected = "Gasolina Premium"
+        XCTAssertEqual(actual, expected, "Expected '\(actual)' to be '\(expected)'")
+    }
+    
+    
+    func testToDictionary_Price() {
+        let actual = mappedDictionary[Fuel.kPrice()] as Double
+        let expected = 222.52
+        XCTAssertEqual(actual, expected, "Expected '\(actual)' to be '\(expected)'")
+    }
+    
+    
+    func testToDictionary_PublishedAt() {
+        let actual = mappedDictionary[Fuel.kPublishedAt()] as NSDate
+        let expected = fuel.publishedAt!
         XCTAssertEqual(actual, expected, "Expected '\(actual)' to be '\(expected)'")
     }
 }
