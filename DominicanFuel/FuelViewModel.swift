a2d417aadb48074774cdbb13c63d1ff3db8f09e2
Julian Tejera <juliantejera@outlook.com>
2015-03-25 23:27:19 -0400

    Added fuel view model, still need to implement published at which depends on the backend's date format

---
+++ DominicanFuel/FuelViewModel.swift
@@ -0,0 +1,24 @@
+//
+//  FuelViewModel.swift
+//  DominicanFuel
+//
+//  Created by Julian Tejera on 3/25/15.
+//  Copyright (c) 2015 Julian Tejera. All rights reserved.
+//
+
+import Foundation
+
+class FuelViewModel {
+    var name = ""
+    var price = ""
+    var publishedAt = ""
+    
+    init(fuel: Fuel) {
+        let numberFormatter = NSNumberFormatter()
+        numberFormatter.numberStyle = NSNumberFormatterStyle.CurrencyStyle
+        let dateFormatter = NSDateFormatter()
+        self.name = fuel.name
+        self.price = numberFormatter.stringFromNumber(fuel.price) ?? "$0.00"
+        self.publishedAt = fuel.publishedAt != nil ? dateFormatter.stringFromDate(fuel.publishedAt!) : ""
+    }
+}
