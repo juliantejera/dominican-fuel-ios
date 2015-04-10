bc44ec2c43bd545c76c7223cefc45fa7a979d341
Julian Tejera <juliantejera@outlook.com>
2015-04-09 22:41:26 -0400

    Added FuelTimeSpanTVC and SplitViewController support for iPad

--- DominicanFuel/FuelTimeSpanTableViewController.swift
+++ DominicanFuel/FuelTimeSpanTableViewController.swift
@@ -8,22 +8,19 @@
 
 import UIKit
 
-class FuelTimeSpanTableViewController: UITableViewController, ManagedDocumentCoordinatorDelegate, UIPopoverPresentationControllerDelegate {
+class FuelTimeSpanTableViewController: UITableViewController, UIPopoverPresentationControllerDelegate {
 
-    var document: UIManagedDocument?
     var dates = [NSDate]()
     
     lazy var dateFormatter: NSDateFormatter = {
-       var formatter = NSDateFormatter()
-       formatter.dateStyle = NSDateFormatterStyle.MediumStyle
+        var formatter = NSDateFormatter()
+        formatter.dateStyle = NSDateFormatterStyle.MediumStyle
         return formatter
     }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        let documentCoordinator = DominicanFuelManagedDocumentCoordinator()
-        documentCoordinator.delegate = self
-        documentCoordinator.setupDocument()
+        populateDates()
     }
 
     override func didReceiveMemoryWarning() {
@@ -31,43 +28,16 @@ class FuelTimeSpanTableViewController: UITableViewController, ManagedDocumentCoo
         // Dispose of any resources that can be recreated.
     }
     
-    
-    // MARK: - Managed Document Coordinator Delegate
-    
-    func managedDocumentCoordinator(coordinator: ManagedDocumentCoordinator, didOpenDocument document: UIManagedDocument) {
-        self.document = document
-        refresh()
+    func populateDates() {
+        let oneYearInWeeks = 52
+        let oneWeekInSeconds: NSTimeInterval = 60*60*24*7
+        var saturday = NSDate.lastSaturday()
+        do {
+            dates.append(saturday)
+            saturday = NSDate(timeInterval: -oneWeekInSeconds, sinceDate: saturday)
+        } while (dates.count < oneYearInWeeks)
     }
     
-    func managedDocumentCoordinator(coordinator: ManagedDocumentCoordinator, didFailWithError error: NSError) {
-        // Handle error
-        println("Error: \(error)")
-    }
-    
-    func refresh() {
-        if let managedObjectContext = document?.managedObjectContext {
-            let request = NSFetchRequest(entityName: Fuel.entityName())
-            request.propertiesToFetch = ["publishedAt"]
-            request.resultType = NSFetchRequestResultType.DictionaryResultType
-            request.returnsDistinctResults = true
-            var publishedAtDescending = NSSortDescriptor(key: "publishedAt", ascending: false, selector: "compare:")
-            request.sortDescriptors = [publishedAtDescending]
-            
-            var error: NSError? = nil
-            if let result = managedObjectContext.executeFetchRequest(request, error: &error) as? [[String: NSDate]] {
-                for dictionary in result {
-                    for (key, date) in dictionary {
-                        dates.append(date)
-                    }
-                }
-                self.tableView.reloadData()
-            } else {
-                println("Error: \(error)")
-            }
-        }
-    }
-    
-
     // MARK: - Table view data source
     
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -78,15 +48,13 @@ class FuelTimeSpanTableViewController: UITableViewController, ManagedDocumentCoo
         let cell = tableView.dequeueReusableCellWithIdentifier("TimespanCell", forIndexPath: indexPath) as UITableViewCell
        
         var date = dates[indexPath.row]
-        var effectiveUntil = NSDate(timeInterval: 60*60*24*6, sinceDate: date)
+        let sixDaysInSeconds: NSTimeInterval = 60*60*24*6
+        var effectiveUntil = NSDate(timeInterval: sixDaysInSeconds, sinceDate: date)
         cell.textLabel?.text = "\(dateFormatter.stringFromDate(date)) - \(dateFormatter.stringFromDate(effectiveUntil))"
         
         return cell
     }
 
-
-
-    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
@@ -95,7 +63,6 @@ class FuelTimeSpanTableViewController: UITableViewController, ManagedDocumentCoo
         // Pass the selected object to the new view controller.
         if segue.identifier == "FuelsSegue" {
             if let vc = segue.destinationViewController.contentViewController as? FuelsTableViewController {
-                vc.document = self.document
                 if let cell = sender as? UITableViewCell {
                     if let indexPath = self.tableView.indexPathForCell(cell) {
                         vc.selectedDate = self.dates[indexPath.row]
