e1a6a01c149823e7baf6f323ad34d89277c4475d
Julian Tejera <juliantejera@outlook.com>
2015-04-15 20:36:15 -0400

    Moved selectedFuelFilters to the right place

--- DominicanFuel/FuelFilterExtension.swift
+++ DominicanFuel/FuelFilterExtension.swift
@@ -14,4 +14,19 @@ extension FuelFilter {
     class func entityName() -> String {
         return "FuelFilter"
     }
+    
+    
+    class func selectedFuelFilters(managedObjectContext: NSManagedObjectContext) -> [FuelFilter] {
+        let request = NSFetchRequest(entityName: FuelFilter.entityName())
+        request.predicate = NSPredicate(format: "isSelected == 1", argumentArray: nil)
+        var error: NSError? = nil
+        
+        if let fuelFilters = managedObjectContext.executeFetchRequest(request, error: &error) as? [FuelFilter] {
+            return fuelFilters
+        } else {
+            println("Error: \(error)")
+        }
+        
+        return [FuelFilter]()
+    }
 }
