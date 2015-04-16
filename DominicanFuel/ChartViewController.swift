7af3049aea26def67d68eda6c2e80702baca19a6
Julian Tejera <juliantejera@outlook.com>
2015-04-15 20:39:26 -0400

    Added fetching code

--- DominicanFuel/ChartViewController.swift
+++ DominicanFuel/ChartViewController.swift
@@ -8,25 +8,58 @@
 
 import UIKit
 
-class ChartViewController: UIViewController {
+class ChartViewController: UIViewController, ManagedDocumentCoordinatorDelegate {
 
+    var document: UIManagedDocument?
     var selectedDate: NSDate = NSDate.lastSaturday() {
         didSet {
-            
+            reloadChart()
         }
     }
     
+    lazy var dateFormatter: NSDateFormatter = {
+        var formatter = NSDateFormatter()
+        formatter.dateStyle = NSDateFormatterStyle.MediumStyle
+        return formatter
+        }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        let documentCoordinator = DominicanFuelManagedDocumentCoordinator()
+        documentCoordinator.delegate = self
+        documentCoordinator.setupDocument()
         
-        // Do any additional setup after loading the view.
     }
     
-    override func viewDidAppear(animated: Bool) {
-        super.viewDidAppear(animated)
-    }
-    
+   
+    func reloadChart() {
+        if let managedObjectContext = document?.managedObjectContext {
+            let request = NSFetchRequest(entityName: Fuel.entityName())
+            let selectedFuelFiltersTypes = FuelFilter.selectedFuelFilters(managedObjectContext).map({ $0.type })
+            if selectedFuelFiltersTypes.count > 0 {
+                request.predicate = NSPredicate(format: "publishedAt >= %@ AND type IN %@", selectedDate.beginningOfDay, selectedFuelFiltersTypes)
+            }
+            let publishedAtAscending = NSSortDescriptor(key: "publishedAt", ascending: true, selector: "compare:")
+            request.sortDescriptors = [publishedAtAscending]
+            var error: NSError? = nil
+            
+            if let results = managedObjectContext.executeFetchRequest(request, error: &error) as? [Fuel] {
 
+            }
+        }
+    }
+    
+    // MARK: - Managed Document Coordinator Delegate
+    
+    func managedDocumentCoordinator(coordinator: ManagedDocumentCoordinator, didOpenDocument document: UIManagedDocument) {
+        self.document = document
+        reloadChart()
+    }
+    
+    func managedDocumentCoordinator(coordinator: ManagedDocumentCoordinator, didFailWithError error: NSError) {
+        // Handle error
+        println("Error: \(error)")
+    }
     
     /*
     // MARK: - Navigation
