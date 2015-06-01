b1b767591cd9f47cbe8144db3c69fcd8bbbb9a2a
Julian Tejera <juliantejera@outlook.com>
2015-05-31 21:01:47 -0400

    Modified LaunchScreen.xib. Now showing an empty Table View Controller

---
+++ DominicanFuel/TableViewCellViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  TableViewCellViewModel.swift
+//  DominicanFuel
+//
+//  Created by Julian Tejera on 5/29/15.
+//  Copyright (c) 2015 Julian Tejera. All rights reserved.
+//
+
+import Foundation
+
+
+class TableViewCellViewModel {
+    var title: String?
+    var subtitle: String?
+}
