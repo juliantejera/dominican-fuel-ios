e3bb5b5bed8dbcb36eab8a8ae893ad761b29d429
juliantejera <juliantejera@outlook.com>
2017-12-09 14:13:59 -0500

    Removed constraints warnings

--- DominicanFuel/UIColor.swift
+++ DominicanFuel/UIColor.swift
@@ -9,8 +9,5 @@
 import Foundation
 
 extension UIColor {
-    
-    class func babyBlueColor() -> UIColor {
-        return UIColor(red: 0.0/255.0, green: 122.0/255.0, blue: 122.0/255.0, alpha: 0)
-    }
+
 }
